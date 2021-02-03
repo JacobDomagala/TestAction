210092facd6e19981905973429ca72b320d0ad08
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-03 19:00:36 +0100

    Use async function

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const owner = core.getInput('owner');
-  const repo = core.getInput('repo');
-  const run_id = core.getInput('run_id');
-  console.log(`Going with /repos/${owner}/${repo}/actions/runs/${run_id}/timing !`);
-  const time = (new Date()).toTimeString();
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const run_id = core.getInput('run_id');
+    console.log(`Going with /repos/${owner}/${repo}/actions/runs/${run_id}/timing !`);
+    const time = (new Date()).toTimeString();
 
 
-  [rv] = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/timing', {
-    owner: owner,
-    repo: repo,
-    run_id: run_id
-  })
+    [rv] = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/timing', {
+      owner: owner,
+      repo: repo,
+      run_id: run_id
+    })
 
-  console.log(`${rv}`)
+    console.log(`${rv}`)
 
-  core.setOutput("time", time);
-} catch (error) {
-  core.setFailed(error.message);
+    core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
