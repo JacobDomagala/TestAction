0aa012c26065fa1a1917fd9ccb977fba4ebc9d37
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-03 18:49:27 +0100

    Add code to get time duration of action

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const owner = core.getInput('owner');
+  const repo = core.getInput('repo');
+  const run_id = core.getInput('run_id');
+  console.log(`Going with /repos/${owner}/${repo}/actions/runs/${run_id}/timing !`);
   const time = (new Date()).toTimeString();
+
+
+  [rv] = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/timing', {
+    owner: owner,
+    repo: repo,
+    run_id: run_id
+  })
+
+  console.log(`${rv}`)
+
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
