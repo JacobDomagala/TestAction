74354c635777a8d11458cdb98d872189ec0e02f3
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-03 20:19:13 +0100

    Obtain octokit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/request");
 
 async function run() {
   try {
@@ -10,14 +11,13 @@ async function run() {
     console.log(`Going with /repos/${owner}/${repo}/actions/runs/${run_id}/timing !`);
     const time = (new Date()).toTimeString();
 
-
-    [rv] = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/timing', {
+    const result = await request('GET /repos/{owner}/{repo}/actions/runs/{run_id}/timing', {
       owner: owner,
       repo: repo,
       run_id: run_id
     })
 
-    console.log(`${rv}`)
+    console.log(`${result}`)
 
     core.setOutput("time", time);
   } catch (error) {
