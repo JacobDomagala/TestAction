a0b4980524cd86b9d6f965afd0cbcbed0da27f89
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-15 22:42:50 +0100

    Update python script

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN chmod +x /generate_graph.py
 # Install all required packages like git/python/matplotlib
 RUN apt-get update && apt-get install python3 -y && \
     apt-get install python3-pip -y && apt-get install git -y
-RUN pip3 install matplotlib
+RUN pip3 install matplotlib PyGithub requests
 
 ENTRYPOINT ["/entrypoint.sh"]
--- generate_graph.py
+++ generate_graph.py
@@ -3,6 +3,32 @@ import random
 import matplotlib
 import matplotlib.pyplot as plt
 import argparse
+from github import Github
+import requests
+import os
+from pprint import pprint
+
+token = os.getenv('GH_TOKEN', '...')
+repo_name = os.getenv('GITHUB_REPOSITORY')
+g = Github(token)
+repo = g.get_repo(f"{repo_name}")
+workflow = repo.get_workflow(id_or_name="test.yml")
+print(f"Workflow ID = {workflow.id}")
+worflow_runs = repo.get_workflow_runs()
+
+
+query_url = f"https://api.github.com/repos/{repo_name}/actions/workflows/{workflow.id}/runs"
+print(f"Query URL = {query_url}")
+# params = {
+#     "state": "open",
+# }
+# headers = {'Authorization': f'token {token}'}
+r = requests.get(query_url)
+pprint(r.json())
+
+
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o','--output',help='Output file name', required=True)
